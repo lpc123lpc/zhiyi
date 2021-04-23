03a507f53da19de6556414650b1522b0c79b1142
朱正阳 <18373110@buaa.edu.cn>
2021-04-23 23:14:35 +0800

    createTable

---
+++ database/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    os.system("python static/createTable.py")
+    app.run()
