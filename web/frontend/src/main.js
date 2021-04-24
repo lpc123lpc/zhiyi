4b1e1ac4012ac38d7cb323422fff7e975613e099
贺祺祥 <18373339@buaa.edu.cn>
2021-04-24 23:49:54 +0800

    vue frame structure close #21

--- web/frontend/src/main.js
+++ web/frontend/src/main.js
@@ -1,16 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Home from './views/Home.vue'
 import 'jquery'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(Home),
+}).$mount('#app')
