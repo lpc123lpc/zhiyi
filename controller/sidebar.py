af95506cecc57d9c162a51afe51186db52f30ac3
LiuQian <liuqian9961@163.com>
2021-04-25 21:28:00 +0800

    对导航栏接口的描述进行了更新

--- controller/sidebar.py
+++ controller/sidebar.py
@@ -4,11 +4,70 @@ import datetime
 import json
 
 
-def getVaccinationSiderbar():
+def getVaccinationSidebar(country):
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
-    vaccined, coverage = [], []
-    todayData = dao.getVacMessage('world', today)
-    yesData = dao.getVacMessage('world', yesterday)
+    todayData = dao.getVacMessage(country, today)
+    yesData = dao.getVacMessage(country, yesterday)
+    return jsonify({
+        "vaccined": {
+            {
+                "date": today,
+                "value": getattr(todayData, 'totalNum')
+            },
+            {
+                "date": yesterday,
+                "value": getattr(yesData, 'totalNum')
+            }
+        },
+        "coverage": {
+            {
+                "date": today,
+                "value": getattr(todayData, 'vacRate')
+            },
+            {
+                "date": yesterday,
+                "value": getattr(yesData, 'vacRate')
+            }
+        }
+    })
 
+
+def getInfectionSidebar(country):
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+    todayData = dao.getInfMessage(country, today)
+    yesData = dao.getInfMessage(country, yesterday)
+    return jsonify({
+        "confirmed": {
+            {
+                "date": today,
+                "value": getattr(todayData, 'totalNum')
+            },
+            {
+                "date": yesterday,
+                "value": getattr(yesData, 'totalNum')
+            }
+        },
+        "death": {
+            {
+                "date": today,
+                "value": getattr(todayData, 'totalDead')
+            },
+            {
+                "date": yesterday,
+                "value": getattr(yesData, 'totalDead')
+            }
+        },
+        "cured": {
+            {
+                "date": today,
+                "value": getattr(todayData, 'cured')
+            },
+            {
+                "date": yesterday,
+                "value": getattr(yesData, 'cured')
+            }
+        }
+    })
 
