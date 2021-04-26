46aae30ee25ed8907062ce30bf4c21695ee3fa98
Gracia <18373420@buaa.edu.cn>
2021-04-26 12:35:56 +0800

    增加测试文件并更新地图显示 close #46

--- web/frontend/src/router/index.js
+++ web/frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 
 const Home = resolve => require(['@/views/Home'], resolve)
@@ -8,13 +7,6 @@ const Home = resolve => require(['@/views/Home'], resolve)
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ],
   routes: [
       {
           path: '/',
