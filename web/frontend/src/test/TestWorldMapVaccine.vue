e1bee1d0d68e42ee676ef5990bd42711d477e52f
Gracia <18373420@buaa.edu.cn>
2021-04-26 19:58:50 +0800

    更新世界地图模块 以万为人数单位

--- web/frontend/src/test/TestWorldMapVaccine.vue
+++ web/frontend/src/test/TestWorldMapVaccine.vue
@@ -6,26 +6,15 @@
 import WorldMapVaccine from '../components/WorldMapVaccine.vue'
 
 export default {
-  name: 'App',
+  name: 'TestWorldMapVaccine',
   components: {
     WorldMapVaccine
   },
   data() {
     return {
-      data : {vaccined: [{name: '中国', value: 4800000}, {name: '加拿大', value: 10000}], 
+      data : {vaccined: [{name: '中国', value: 22030.9}, {name: '加拿大', value: 10}], 
               coverage: [{name: '中国', value: 0.111}]}
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
