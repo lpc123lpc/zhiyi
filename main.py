3d99e49f70e79bd89f33189cb22d911921898b98
LiuQian <liuqian9961@163.com>
2021-04-29 11:39:59 +0800

    uodate main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import template_rendered
 from controller import map, table, sidebar
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/')
