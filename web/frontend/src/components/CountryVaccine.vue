cbbac19716a3080df9a2456f0980f5d2bcfb2efb
李芃村 <18373358@buaa.edu.cn>
2021-04-29 23:22:24 +0800

    世界表格排序，国家接种情况 close #30

---
+++ web/frontend/src/components/CountryVaccine.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="countryVaccine" style="width: auto;height:600px;"></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  name: 'CountryVaccine',
+  mounted () {
+    this.CountryVaccine()
+  },
+  methods: {
+    CountryVaccine () {
+      var myChart = echarts.init(document.getElementById('countryVaccine'))
+      var series = []
+      var names = []
+      fetch('../static/json/charts/testVaccine').then(function (response) {
+        response.json().then(function (data) {
+          for (var i = 0; i < data.length; i++) {
+            series.push(
+              {
+                name: data[i].name,
+                type: 'line',
+                data: data[i].vaccined
+              }
+            )
+            names.push(data[i].name)
+          }
+          myChart.setOption({
+            title: {
+              text: data[0].name + '接种数据',
+              textStyle: {
+                color: '#333'
+              }
+            },
+            tooltip: {
+              trigger: 'axis'
+            },
+            legend: {
+              data: names
+            },
+            xAxis: [{
+              name: '日期',
+              type: 'category',
+              data: data[0].time
+            }],
+            yAxis: [
+              {
+                name: '接种人数',
+                type: 'value'
+              }
+            ],
+            dataZoom: {
+              type: 'inside'
+            },
+            series: series
+          })
+        })
+      }).catch(function (err) {
+        alert(err.toString())
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- web/frontend/src/components/WorldTable.vue
+++ web/frontend/src/components/WorldTable.vue
@@ -1,53 +1,56 @@
 <template>
     <div id = 'WorldTable'>
-      <table class="table table-hover" id="world">
-        <thead>
-          <tr>
-            <th>国家名称</th>
-            <th >累计确诊人数</th>
-            <th>新增确诊人数</th>
-            <th>累计治愈人数</th>
-            <th>累计死亡人数</th>
-            <th>累计接种人数</th>
-            <th>新增接种人数</th>
-            <th>接种率</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item) in items" :key="item.name">
-            <td>{{item.name}}</td>
-            <td>{{item.confirmed}}</td>
-            <td>{{item.newConfirmed}}</td>
-            <td>{{item.cured}}</td>
-            <td>{{item.deceased}}</td>
-            <td>{{item.vaccined}}</td>
-            <td>{{item.newVaccined}}</td>
-            <td>{{item.vaccined_coverage}}</td>
-          </tr>
-        </tbody>
-      </table>
+      <el-table :data="items" border highlight-current-row>
+        <el-table-column
+          prop="name"
+          label="国家">
+        </el-table-column>
+        <el-table-column
+          prop="confirmed"
+          label="累计确诊人数"
+          sortable>
+        </el-table-column>
+        <el-table-column
+          prop="newConfirmed"
+          label="新增确诊人数"
+          sortable>
+        </el-table-column>
+        <el-table-column
+          prop="cured"
+          label="累计治愈人数"
+          sortable>
+        </el-table-column>
+        <el-table-column
+          prop="deceased"
+          label="累计死亡人数"
+          sortable>
+        </el-table-column>
+        <el-table-column
+          prop="vaccined"
+          label="累计接种人数"
+          sortable>
+        </el-table-column>
+        <el-table-column
+          prop="newVaccined"
+          label="新增接种人数"
+          sortable>
+        </el-table-column>
+        <el-table-column
+          prop="vaccined_coverage"
+          label="接种率"
+          sortable>
+        </el-table-column>
+      </el-table>
     </div>
 </template>
 
 <script>
-import $ from 'jquery'
-import 'datatables/media/js/jquery.dataTables.js'
 export default {
 
   name: 'WorldTable',
   data: function () {
     return {
       items: []
-    }
-  },
-  watch: {
-    items: function () {
-      this.$nextTick(function () {
-        $('#world').dataTable({
-          bSort: 'true',
-          bRetrieve: 'true'
-        })
-      })
     }
   },
   mounted () {
