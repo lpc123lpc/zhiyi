67a0e3fcd96d806d59a6e075847e5819ee3d3245
LiuQian <liuqian9961@163.com>
2021-05-08 18:04:36 +0800

    修改地图显示bug

--- controller/map.py
+++ controller/map.py
@@ -8,12 +8,15 @@ infdata = testData.infdata'''
 
 
 def getMapVaccine(country):
+    a = 1.0
+    if country == 'global':
+        a = 10000.0
     data = dao.getNowVacMessageInclude(country)
     vaccined, coverage = [], []
     if data is None:
         return jsonify({})
     for i in data:
-        vaccined.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'totalNum') / 10000.0})
+        vaccined.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'totalNum') / a})
         coverage.append({"name": getattr(i, 'areaName'), "vacRate": getattr(i, 'vacRate')})
     return jsonify({
         "vaccined": vaccined,
@@ -22,15 +25,18 @@ def getMapVaccine(country):
 
 
 def getMapInfection(country):
+    a = 1.0
+    if country == 'global':
+        a = 10000.0
     data = dao.getNowInfMessageInclude(country)
     nowConfirm, totalConfirm, cured, dead = [], [], [], []
     if data is None:
         return jsonify({})
     for i in data:
-        nowConfirm.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'currentNum') / 10000.0})
-        totalConfirm.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'totalNum') / 10000.0})
-        cured.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'cured') / 10000.0})
-        dead.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'totalDead') / 10000.0})
+        nowConfirm.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'currentNum') / a})
+        totalConfirm.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'totalNum') / a})
+        cured.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'cured') / a})
+        dead.append({"name": getattr(i, 'areaName'), "value": getattr(i, 'totalDead') / a})
     return jsonify({
         "nowConfirm": nowConfirm,
         "totalConfirm": totalConfirm,
