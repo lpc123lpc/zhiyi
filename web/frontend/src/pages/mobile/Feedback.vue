d8fffad7f4eade7def6c36442986669720e86563
贺祺祥 <18373339@buaa.edu.cn>
2021-06-05 17:16:23 +0800

    finish feedaback page close #121

--- web/frontend/src/pages/mobile/Feedback.vue
+++ web/frontend/src/pages/mobile/Feedback.vue
@@ -1,13 +1,18 @@
 <template>
-  <page-header v-bind:header_title="'反馈&建议'"></page-header>
+  <div>
+    <page-header v-bind:header_title="'反馈&建议'"></page-header>
+    <feedback-content class="feed-back-content-item"></feedback-content>
+  </div>
 </template>
 
 <script>
 import pageHeader from '../../components/mobile/PageHeader.vue'
+import feedBackContent from '../../components/mobile/FeedbackInput.vue'
 export default {
   name: 'Feedback',
   components: {
-    'page-header': pageHeader
+    'page-header': pageHeader,
+    'feedback-content': feedBackContent
   }
 }
 </script>
