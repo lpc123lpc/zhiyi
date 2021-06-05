c41cc94b1364b661d754ef339474d8082a38e9ea
Gracia <18373420@buaa.edu.cn>
2021-06-05 17:42:07 +0800

    首页修改 close #95

---
+++ web/frontend/src/assets/js/swiper.js
@@ -0,0 +1,15 @@
+// 轮播图
+const swiperList = [{
+  picImg: require('../../../static/image/Cover.png')
+}, {
+  picImg: require('../../../static/image/swiper/1.png')
+}, {
+  picImg: require('../../../static/image/swiper/5.png')
+}, {
+  picImg: require('../../../static/image/swiper/3.png')
+}, {
+  picImg: require('../../../static/image/swiper/4.png')
+}];
+export {
+  swiperList
+}
--- web/frontend/src/pages/pc/Cover.vue
+++ web/frontend/src/pages/pc/Cover.vue
@@ -1,27 +1,72 @@
 <template>
-  <el-container>
-  <el-header></el-header>
-  <el-main>
-    <div class="block" style="text-align: center">
-      <el-image src='../../../static/image/Cover.png'></el-image>
-    </div>
+  <!--  <el-container>-->
+  <!--  <el-header></el-header>-->
+  <!--  <el-main>-->
+  <!--    <div class="block" style="text-align: center">-->
+  <!--      <el-image src='../../../static/image/Cover.png'></el-image>-->
+  <!--    </div>-->
+  <!--    <div style="text-align: center">-->
+  <!--      <el-button type="primary" @click="goHome()" plain style="height: 80px;width: 300px;font-size: 30px">开始</el-button>-->
+  <!--    </div>-->
+  <!--  </el-main>-->
+  <!--  <el-footer>-->
+  <!--    <div style="text-align: center">-->
+  <!--      <el-link type="info" href="https://beian.miit.gov.cn/">京ICP备2021014159号</el-link>-->
+  <!--    </div>-->
+  <!--  </el-footer>-->
+  <!--</el-container>-->
+  <div>
+    <!--    <div class="image-menu-item">-->
+    <!--      <el-image src='../../../static/image/Cover.png'></el-image>-->
+    <!--    </div>-->
+    <el-row style="margin-bottom: 40px">
+      <el-col :span="2">
+        <img src='../../../static/image/top-icon.jpg' style="height: 100px"/>
+      </el-col>
+      <el-col :span="22" class="title">
+        <span>知疫——疫情与疫苗可视化平台</span>
+      </el-col>
+    </el-row>
+    <el-carousel
+      :interval="2000"
+      type="card"
+      class="carousel">
+      <el-carousel-item v-for="(item, index) in swiperList" :key="index" class="el-carousel__item">
+        <img :src="item.picImg"/>
+      </el-carousel-item>
+    </el-carousel>
     <div style="text-align: center">
-      <el-button type="primary" @click="goHome()" plain style="height: 80px;width: 300px;font-size: 30px">开始</el-button>
+      <el-button type="primary" @click="goHome()" plain class="button">开始</el-button>
     </div>
-  </el-main>
-  <el-footer>
-    <div style="text-align: center">
-      <el-link type="info" href="https://beian.miit.gov.cn/">京ICP备2021014159号</el-link>
-    </div>
-  </el-footer>
-</el-container>
+    <el-footer>
+      <div style="text-align: center; margin-top: 5px">
+        <el-link type="info" href="https://beian.miit.gov.cn/">京ICP备2021014159号</el-link>
+      </div>
+    </el-footer>
+  </div>
 </template>
 
 <script>
+import {swiperList} from "../../assets/js/swiper";
+
 export default {
   name: 'Cover',
+  data() {
+    return {
+      swiperList: []
+    }
+  },
+  created() {
+    this.swiperList = swiperList
+  },
+  // mounted() {
+  //   document.body.style.backgroundColor = 'rgba(167,219,255,0.64)'
+  // },
+  // beforeDestroy() {
+  //   document.querySelector('body').removeAttribute('style')
+  // },
   methods: {
-    goHome () {
+    goHome() {
       this.$router.push({path: `/Home`})
     }
   }
@@ -29,5 +74,5 @@ export default {
 </script>
 
 <style scoped>
-
+@import "../../assets/css/cover.css";
 </style>
