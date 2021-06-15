d668853ded950ea594eb0b89adced8354ecd4e54
Gracia <18373420@buaa.edu.cn>
2021-06-15 17:18:22 +0800

    首页图片显示

--- web/frontend/src/assets/js/swiper.js
+++ web/frontend/src/assets/js/swiper.js
@@ -2,11 +2,17 @@
 const swiperList = [{
   picImg: require('../../../static/image/Cover.png')
 }, {
-  picImg: require('../../../static/image/swiper/1.png')
+  picImg: require('../../../static/image/swiper/5.png')
 }, {
   picImg: require('../../../static/image/swiper/2.png')
 }, {
   picImg: require('../../../static/image/swiper/3.png')
+}, {
+  picImg: require('../../../static/image/swiper/7.png')
+}, {
+  picImg: require('../../../static/image/swiper/1.png')
+}, {
+  picImg: require('../../../static/image/swiper/6.png')
 }, {
   picImg: require('../../../static/image/swiper/4.png')
 }];
